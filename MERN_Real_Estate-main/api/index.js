c267d05e4c552dba14da334323def36d70138fd9
qamar345 <qamargill427@gmail.com>
2023-10-17 20:47:12 +0500

    create a middleware and function to handle error

--- MERN_Real_Estate-main/api/index.js
+++ MERN_Real_Estate-main/api/index.js
@@ -20,6 +20,16 @@ app.use(express.json())
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
+
 
 app.listen(9900, () => {
     console.log("Server is Running on Port 9900")
