4b31a11f3a096695c358804abbd31746a6180319
qamar345 <qamargill427@gmail.com>
2023-10-28 22:17:51 +0500

    update user api

--- MERN_Real_Estate-main/api/controllers/user.controller.js
+++ MERN_Real_Estate-main/api/controllers/user.controller.js
@@ -1,5 +1,33 @@
+import { errorHandler } from "../utils/error.js"
+import User from "../models/user.model.js";
+import bcriptjs from 'bcryptjs';
+
 export const test = (req, res) => {
     res.json({
         name: "API router is working",
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "Unauthorized User"));
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcriptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, { new: true });
+
+        const { password, ...rest } = updateUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
