2d573db3b009730b5a72f5a479605d55dc353437
qamar345 <qamargill427@gmail.com>
2023-10-29 15:55:47 +0500

    create listing api

--- MERN_Real_Estate-main/api/index.js
+++ MERN_Real_Estate-main/api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.router.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
